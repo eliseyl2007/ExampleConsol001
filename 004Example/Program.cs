516e4db382f2a4f03e61f1cb93f2e7b75be057f0
Elisei <elisey-l@mail.ru>
2023-09-07 21:00:34 +0300

    Fourth02

--- 004Example/Program.cs
+++ 004Example/Program.cs
@@ -1,4 +1,6 @@
-﻿double numberA = 18;
-double numberB = 5;
-double answer = numberA / numberB;
+﻿int numberA = new Random().Next(1,10);// 1 2 3 4 5 .... 10
+Console.WriteLine(numberA);
+int numberB = new Random ().Next(1,10);
+Console.WriteLine(numberB);
+int answer = numberA + numberB;
 System.Console.WriteLine(answer);
