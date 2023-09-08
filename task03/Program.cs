9e7606b5eeab51ddcfd640afeec7c92122571fb0
Elisei <elisey-l@mail.ru>
2023-09-08 17:33:32 +0300

    2

---
+++ task03/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите номер дня недели от 1 до 7");
+string num = Console.ReadLine();
+if (num == "1")Console.WriteLine("Понедельник");
+if (num == "2")Console.WriteLine("Вторник");
+if (num == "3")Console.WriteLine("Среда");
+if (num == "4")Console.WriteLine("Четверг");
+if (num == "5")Console.WriteLine("Пятница");
+if (num == "6")Console.WriteLine("Суббота");
+if (num == "7")Console.WriteLine("Воскресение");
