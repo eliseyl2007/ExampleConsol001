f45152873c75b6a06d721a4dc036a4dc408e65a6
Elisei <elisey-l@mail.ru>
2023-09-08 17:58:47 +0300

    5

---
+++ task05/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите целое положительное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = -num;
+while (count <= num);
+{
+    Console.Write($"{count} ");
+    count++;
+}
